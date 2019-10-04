17228e4ac780e9d6df2cbefec0a68ebbaa497311
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-10-04 18:54:37 +0700

    access token and refresh token

--- main.go
+++ main.go
@@ -11,5 +11,16 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	h := &handler{}
+
+	e.POST("/login", h.login)
+
+	e.GET("/private", h.private, isLoggedIn)
+
+	e.GET("/admin", h.private, isLoggedIn, isAdmin)
+
+	e.POST("/token", h.token)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
